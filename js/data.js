08e38b286d9b8a0eb0d6b5749c3af1c969aebf13
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2026-02-13 22:57:27 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@
 let users = JSON.parse(localStorage.getItem('users')) || [
   { id: 1, name: "Thistle Caindoy", email: "thistle_zhi_caindoy@dlsu.edu.ph",password:"thisle123", role: "student", description: "I am a BSMS - Computer Science Student." },
   { id: 2, name: "Luis Carlos", email: "luis_carlos@dlsu.edu.ph", password:"luis123", role: "student", description: "Computer Science student who loves coding." },
-  { id: 3, name: "Ramuel Cordero", email: "ramuel_cordero@dlsu.edu.ph", password:"ram123", role: "student", description: "I enjoy working using css!" },
+  { id: 3, name: "Ramuel Cordero", email: "ramuel_cordero@dlsu.edu.ph", password:"ram123", role: "student", description: "I enjoy working with css!" },
   { id: 4, name: "Alberto Descalzo", email: "alberto_descalzo@dlsu.edu.ph", password:"albert123", role: "student", description: "I prefer quiet computer laboratories." },
   { id: 5, name: "Danny Cheng", email: "danny.cheng@dlsu.edu.ph", password:"dan123", role: "technician", description: "Lab technician assisting students." }
 ];
