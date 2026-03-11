426cdfca68258b9d0ac583d4065eef7406aa702f
Ramuel Cordero <virkato71@gmail.com>
2026-03-12 02:05:39 +0800

    server.js fix

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'view')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'login.html'));
+    res.sendFile(path.join(__dirname, 'view', 'login.html'));
 });
 
 app.listen(PORT, () => {
