bb740a7f69337e0aa52a259f0803e150764342a9
Wijiwise <luis.guiller.carlos@gmail.com>
2026-03-12 15:26:25 +0800

    Refactor Reservations to use Mongoose schema

--- CCAPDEV-MCO-main/model/Reservations.js
+++ CCAPDEV-MCO-main/model/Reservations.js
@@ -1,58 +1,20 @@
 // RESERVATIONS
 
-let reservations = [
-  {
-    id: 1,
-    userId: 2,
-    labId: 1,
-    seat: 3,
-    date: "2026-03-15",
-    time: "09:00",
-    anonymous: false,
-    dateRequested: "2026-01-28 14:32"
-  },
-  {
-    id: 2,
-    userId: 1,
-    labId: 2,
-    seat: 1,
-    date: "2026-03-14",
-    time: "10:00",
-    anonymous: true,
-    dateRequested: "2026-01-28 15:02"
-  },
-  {
-    id: 3,
-    userId: 4,
-    labId: 3,
-    seat: 9,
-    date: "2026-03-17",
-    time: "11:00",
-    anonymous: false,
-    dateRequested: "2026-02-10 13:00"
-  },
-  {
-    id: 4,
-    userId: 3,
-    labId: 4,
-    seat: 2,
-    date: "2026-03-15",
-    time: "10:30",
-    anonymous: false,
-    dateRequested: "2026-02-08 17:34"
-  },
-  {
-    id: 5,
-    userId: 1,
-    labId: 3,
-    seat: 5,
-    date: "2026-03-16",
-    time: "11:00",
-    anonymous: false,
-    dateRequested: "2026-02-11 14:50"
-  }
-];
+const mongoose = require('mongoose');
 
+const reservationSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  labId: { type: mongoose.Schema.Types.ObjectId, ref: 'Lab', required: true },
+  seat: { type: Number, required: true },
+  date: { type: String, required: true }, // Format: YYYY-MM-DD
+  time: { type: String, required: true }, // Format: HH:mm
+  anonymous: { type: Boolean, default: false },
+  dateRequested: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Reservation', reservationSchema);
+
+/*
 exports.getAll = () => reservations;
 
 exports.getById = (id) =>
@@ -75,4 +37,4 @@ exports.delete = (id) => {
 
 exports.deleteByUser = (userId) => {
     reservations = reservations.filter(r => r.userId !== parseInt(userId));
-};
+};*/
